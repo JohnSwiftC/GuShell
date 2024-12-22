c6d30a1e4b3a3a5135ce9b23bfd9aeb3a2c28fe4
JohnSwiftC <johnswift@live.com>
2024-12-22 15:43:32 -0600

    adds comment

--- GuShellClient/GuShellManager.c
+++ GuShellClient/GuShellManager.c
@@ -24,6 +24,7 @@ int startConversationWithClient(int remoteDesk, char* messageBuf, char* response
 void handleNewConnectionProc(void* pID);
 void printWelcomeMessage();
 
+// Here is my new comment!
 
 // These need to be top level for easy threading. DO NOT CHANGE AFTER STARTING NEW THREADS UNLESS BLOCKED AND ACTUALLY PLANNED OUT
 HANDLE hClientListMutex;
